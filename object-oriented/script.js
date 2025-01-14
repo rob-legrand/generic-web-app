d95698d202dc2d1585abe9c60b27af848f81b754
Rob LeGrand <roblegrand98@gmail.com>
2025-01-14 10:31:56 -0600

    Fix JavaScript to pass newer JSLint

--- object-oriented/script.js
+++ object-oriented/script.js
@@ -1,30 +1,26 @@
-/*jslint browser: true, indent: 3 */
+/*jslint browser */
 
 // Here's a generic web app that uses model/view/controller code organization and web storage.
 
 // All the code below will be run once the page content finishes loading.
 document.addEventListener('DOMContentLoaded', function () {
    'use strict';
-   var createToDoList;
 
    // Create a factory that makes an object to keep track of a to-do list.
-   createToDoList = function (oldState) {
-      var self, state;
+   const createToDoList = function (oldState) {
 
-      // Create a default starting state with an empty to-do list.
-      state = {
-         list: []
-      };
-      // If there's a valid previous state, use it instead.
-      if (typeof oldState === 'string') {
-         try {
-            state = JSON.parse(oldState);
-         } catch (ignore) {
+      // The util object contains private methods.
+      const util = Object.freeze({
+         createNewState: function () {
+            // Return a new state object with an empty to-do list.
+            return {
+               list: []
+            };
          }
-      }
+      });
 
       // The self object contains public methods.
-      self = {
+      const self = Object.freeze({
          addItem: function (item) {
             // Add a new item to the end of the list.
             state.list = state.list.concat(item);
@@ -45,33 +41,45 @@ document.addEventListener('DOMContentLoaded', function () {
             // Remove an item from anywhere in the list.
             state.list = state.list.slice(0, whichItem).concat(state.list.slice(whichItem + 1));
          }
-      };
-      // Normally it's best to freeze the self object to keep it from being modified later.
-      return Object.freeze(self);
+      });
+
+      // Initialize the private state.
+      const state = (function () {
+         // If there's a valid previous state, use it.
+         if (typeof oldState === 'string') {
+            try {
+               return JSON.parse(oldState);
+            } catch {
+            }
+         }
+         // Otherwise, create a default starting state.
+         return util.createNewState();
+      }());
+
+      // Return the frozen self object, full of public methods.
+      return self;
    };
 
    // Create a new closure to hide the view and controller from the model code above.
    (function () {
-      var toDoList, updateToDoList;
 
       // Create a function that updates everything that needs updating whenever the model changes.
-      updateToDoList = function () {
-         var toDoListOutputElement;
+      const updateToDoList = function () {
 
-         // Save the new state in web storage (if available).
+         // Save the new state in web storage.
          localStorage.setItem('generic web app', toDoList.getState());
 
          // Update the view.
-         toDoListOutputElement = document.querySelector('#to-do-list-output');
+         const toDoListOutputElement = document.querySelector('#to-do-list-output');
          // Empty the #to-do-list-output element of all child elements.
          Array.from(toDoListOutputElement.childNodes).forEach(function (childNode) {
             childNode.remove();
          });
          // Insert the list items as new li elements one by one.
-         Array.from({length: toDoList.getNumItems()}, function () {
+         Array.from({length: toDoList.getNumItems()}, () => (
             // Create each new li element in HTML.
-            return document.createElement('li');
-         }).forEach(function (liElement, whichItem) {
+            document.createElement('li')
+         )).forEach(function (liElement, whichItem) {
             // Give it its to-do list item.
             liElement.textContent = toDoList.getItem(whichItem);
             // Insert it just inside the end of the list.
@@ -85,25 +93,24 @@ document.addEventListener('DOMContentLoaded', function () {
                toDoList.removeItem(whichItem);
                // Update everything else based on the new model state.
                updateToDoList();
-            }, false);
+            });
          });
       };
 
       // Set up the controller:  Handle adding a new to-do list item.
       document.querySelector('#add-to-do-list-item').addEventListener('click', function () {
-         var itemToAdd;
          // Update the model.
-         itemToAdd = document.querySelector('#to-do-list-item-to-add').value;
+         const itemToAdd = document.querySelector('#to-do-list-item-to-add').value;
          if (itemToAdd.length > 0) {
             toDoList.addItem(itemToAdd);
          }
          // Update everything else based on the new model state.
          updateToDoList();
-      }, false);
+      });
 
       // When the page is loaded, get any saved state from web storage and use it to create a new model.
-      toDoList = createToDoList(localStorage.getItem('generic web app'));
+      const toDoList = createToDoList(localStorage.getItem('generic web app'));
       // Update everything else based on the new model state.
       updateToDoList();
    }());
-}, false);
+});
